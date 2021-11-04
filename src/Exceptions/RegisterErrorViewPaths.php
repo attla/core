c0035ec709e45b16e243b5c9be6572d2e9b5bfb5
Lucas Nicolau <fedhrm@gmail.com>
2021-11-04 20:13:08 -0300

    🔨 refactoring the namespace filter

--- src/Exceptions/RegisterErrorViewPaths.php
+++ src/Exceptions/RegisterErrorViewPaths.php
@@ -7,14 +7,18 @@ use Illuminate\Support\Facades\View;
 class RegisterErrorViewPaths
 {
     /**
-     * Register the error view paths.
+     * Register the error view paths
      *
      * @return void
      */
     public function __invoke()
     {
         View::replaceNamespace('errors', collect(config('view.paths'))->map(function ($path) {
-            return "{$path}/errors";
-        })->push(__DIR__ . '/views')->all());
+            if (is_dir($errorsPath = "$path/errors")) {
+                return $errorsPath;
+            }
+        })->filter()
+        ->push(__DIR__ . '/views')
+        ->all());
     }
 }

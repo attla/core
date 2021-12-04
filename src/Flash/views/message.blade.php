94ab30055a6e1683956c519f23834b76a12f8086
Lucas Nicolau <fedhrm@gmail.com>
2021-12-03 21:06:23 -0300

    ⚡ improve flash messages

--- src/Flash/views/message.blade.php
+++ src/Flash/views/message.blade.php
@@ -1,7 +1,22 @@
-@if($errors->has('csrf'))
-<div class="alert alert-warning alert-dismissible{{ !empty($last) ? ' ' . $last : '' }}" role="alert">
+@if ($errors->has('csrf'))
+<div @class([
+    'alert alert-warning alert-dismissible',
+    (!empty($last) ? $last : '')            => !empty($last) ? $last : '',
+    'rounded-0'                             => !empty($flat),
+]) role="alert">
     @foreach($errors->get('csrf') as $csrf)
     {!! $csrf !!}
+    @endforeach
+    <button type="button" class="btn-close" data-bs-dismiss="alert" data-bs-toggle="tooltip" title="Esconder" onclick="$('.tooltip').remove()"></button>
+</div>
+@elseif ($errors->any())
+<div @class([
+    'alert alert-danger alert-dismissible',
+    (!empty($last) ? $last : '')            => !empty($last) ? $last : '',
+    'rounded-0'                             => !empty($flat),
+]) role="alert">
+    @foreach($errors->all() as $error)
+    {{ $error }}<br/>
     @endforeach
     <button type="button" class="btn-close" data-bs-dismiss="alert" data-bs-toggle="tooltip" title="Esconder" onclick="$('.tooltip').remove()"></button>
 </div>

6419c22f03b9315d6665a3680779af407ed2ef88
Lucas Nicolau <fedhrm@gmail.com>
2022-02-20 04:43:46 -0300

    ✨ adding AliasLoader for facades

--- src/Bootstrap/RegisterFacades.php
+++ src/Bootstrap/RegisterFacades.php
@@ -3,6 +3,8 @@
 namespace Attla\Bootstrap;
 
 use Attla\Application;
+use Attla\AliasLoader;
+use Attla\PackageDiscover;
 use Illuminate\Support\Facades\Facade;
 
 class RegisterFacades
@@ -17,5 +19,12 @@ class RegisterFacades
     {
         Facade::clearResolvedInstances();
         Facade::setFacadeApplication($app);
+
+        AliasLoader::getInstance(
+            Facade::defaultAliases()
+                ->merge($app['config']->get('aliases', []))
+                ->merge($app[PackageDiscover::class]->aliases())
+                ->toArray()
+        )->register();
     }
 }

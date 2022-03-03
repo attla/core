97fd6ca23334572accfb1439a6cf48555f0d6172
Lucas Nicolau <fedhrm@gmail.com>
2022-03-03 20:49:30 -0300

    ✨ adding --except-vendor option to `router:list`

--- src/Console/Commands/RouteListCommand.php
+++ src/Console/Commands/RouteListCommand.php
@@ -136,6 +136,7 @@ class RouteListCommand extends Command
             'name' => $route->getName(),
             'action' => ltrim($route->getActionName(), '\\'),
             'middleware' => $this->getMiddleware($route),
+            'vendor' => $this->isVendorRoute($route),
         ]);
     }
 
@@ -195,6 +196,32 @@ class RouteListCommand extends Command
     }
 
     /**
+     * Determine if the route has been defined outside of the application
+     *
+     * @param \Illuminate\Routing\Route $route
+     * @return bool
+     */
+    protected function isVendorRoute(Route $route)
+    {
+        if ($route->action['uses'] instanceof Closure) {
+            $path = (new \ReflectionFunction($route->action['uses']))
+                                ->getFileName();
+        } elseif (
+            is_string($route->action['uses']) &&
+                  str_contains($route->action['uses'], 'SerializableClosure')
+        ) {
+            return false;
+        } elseif (is_string($route->action['uses'])) {
+            $path = (new \ReflectionClass(explode('@', $route->action['uses'])[0]))
+                                ->getFileName();
+        } else {
+            return false;
+        }
+
+        return str_starts_with($path, base_path('vendor'));
+    }
+
+    /**
      * Filter the route by URI and / or name
      *
      * @param array $route
@@ -203,10 +230,11 @@ class RouteListCommand extends Command
     protected function filterRoute(array $route)
     {
         if (
-            ($this->option('name') && ! Str::contains($route['name'], $this->option('name'))) ||
-            ($this->option('path') && ! Str::contains($route['uri'], $this->option('path'))) ||
-            ($this->option('method') && ! Str::contains($route['method'], strtoupper($this->option('method')))) ||
-            ($this->option('domain') && ! Str::contains($route['domain'], $this->option('domain')))
+            ($this->option('name') && ! Str::contains($route['name'], $this->option('name')))
+            || ($this->option('path') && ! Str::contains($route['uri'], $this->option('path')))
+            || ($this->option('method') && ! Str::contains($route['method'], strtoupper($this->option('method'))))
+            || ($this->option('domain') && ! Str::contains($route['domain'], $this->option('domain')))
+            || ($this->option('except-vendor') && $route['vendor'])
         ) {
             return;
         }
@@ -423,6 +451,7 @@ class RouteListCommand extends Command
             ['except-path', null, InputOption::VALUE_OPTIONAL, 'Do not display the routes matching the given path pattern'],
             ['reverse', 'r', InputOption::VALUE_NONE, 'Reverse the ordering of the routes'],
             ['sort', null, InputOption::VALUE_OPTIONAL, 'The column (precedence, domain, method, uri, name, action, middleware) to sort by', 'uri'],
+            ['except-vendor', null, InputOption::VALUE_NONE, 'Do not display routes defined by vendor packages'],
         ];
     }
 }
